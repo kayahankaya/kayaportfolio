5727cf7e17f625237d879275cee623d57f64708c
kayahankaya <kayahankaya@icloud.com>
2023-10-10 00:52:30 +0200

    settings

--- kayaportfolio/kayaportfolio/settings.py
+++ kayaportfolio/kayaportfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6b=p-+*b0kyrnwofuzcn!jpvuwsu!ii=kz!r9*uag2=0u!9dma'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["kayahankaya.com"]
 
 
 # Application definition
