1f1801c1c456639c7b13e8896497fdd7e70c99f1
kayahankaya <kayahankaya@icloud.com>
2023-11-16 12:54:00 +0100

    added sublink

--- kayaportfolio/urls.py
+++ kayaportfolio/urls.py
@@ -8,5 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
     path('', views.landingpage_view, name="landingpage_view"),
+    path('sample/', views.sample_view, name="sample_view"),
 ]
 
--- kayaportfolio/views.py
+++ kayaportfolio/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def landingpage_view(request):
     return render(request, 'kayaportfolio/index.html')
+
+def sample_view(request):
+    return render(request, 'kayaportfolio/sample.html')
